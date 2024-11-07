37e525b58e4dc9b2eb9cd34c257d7d2639ac1066
UMA MAHESH <UMA MAHESH@LAPTOP-3RU2HEOP>
2024-11-07 09:19:34 +0530

    written

--- ArunsFramework/src/test/java/createOrganisationTest/ToCreateOrganisationTest.java
+++ ArunsFramework/src/test/java/createOrganisationTest/ToCreateOrganisationTest.java
@@ -3,8 +3,8 @@ package createOrganisationTest;
 public class ToCreateOrganisationTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+System.out.println("orgcreated");
 	}
 
 }
